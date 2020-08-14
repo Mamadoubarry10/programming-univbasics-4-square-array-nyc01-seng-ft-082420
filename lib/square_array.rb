5d29120c78854af918a0d26a2f9f8ae1f6781a2e
mamadou alpha barry <mamadou.barry1105@gmail.com>
2020-08-14 06:11:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
+
 def square_array(array)
   count = 0
-  while count < array.length do
-    square = array[count] ** 2
+  while array[count] do
+    puts array[count] ** 2
     count += 1
   end
 end
-
